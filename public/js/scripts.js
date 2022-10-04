467ec2256a1f0d34dbe960a5600bad6da73a34f6
ThePixelMixery <pixelmixery@gmail.com>
2022-10-04 13:49:13 +1100

    Changed index for login

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
- 
-	var myNav = document.querySelectorAll('.sidenav');
+document.addEventListener("DOMContentLoaded", function () {
+	var myNav = document.querySelectorAll(".sidenav");
 	M.Sidenav.init(myNav, {});
-   
-  });
+});
+
+const loginForm = () => {
+	let loginData = {};
+	loginData.email = $("#email").val();
+	loginData.last_name = $("#password").val();
+
+	console.log("Login Data Submitted: ", loginData);
+};
+
+$(document).ready(function () {
+	$("#loginForm").click(() => {
+		submitForm();
+	});
+	M.updateTextFields();
+});
