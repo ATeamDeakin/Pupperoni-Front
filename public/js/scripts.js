6a2d88c0f7cb77a284398f196c96395e748421ec
akshatarora02 <akshatarora028@gmail.com>
2022-10-05 17:52:49 +1100

    Changed Login API - working now

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,50 +1,50 @@
 var jwt = localStorage.getItem("jwt");
 if (jwt != null) {
-  window.location.href = './match.html'
+	window.location.href = './match.html'
 }
 
 let socket = io();
-socket.on( () => {
-    console.log('Random number: ');
+socket.on(() => {
+	console.log('Random number: ');
 });
 
 function login() {
 	const username = document.getElementById("username").value;
 	const password = document.getElementById("password").value;
-  
+
 	const xhttp = new XMLHttpRequest();
-	xhttp.open("POST", "/login");
+	xhttp.open("POST", "http://localhost:8080/login");
 	xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 	xhttp.send(JSON.stringify({
-	  "username": username,
-	  "password": password
+		"username": username,
+		"password": password
 	}));
 	xhttp.onreadystatechange = function () {
-	  if (this.readyState == 4) {
-		const objects = JSON.parse(this.responseText);
-		console.log(objects);
-		if (objects['status'] == 'ok') {
-		  localStorage.setItem("jwt", objects['accessToken']);
-		  Swal.fire({
-			text: objects['message'],
-			icon: 'success',
-			confirmButtonText: 'OK'
-		  }).then((result) => {
-			if (result.isConfirmed) {
-			  window.location.href = './match.html';
+		if (this.readyState == 4) {
+			const objects = JSON.parse(this.responseText);
+			console.log(objects);
+			if (objects['status'] == 'ok') {
+				localStorage.setItem("jwt", objects['accessToken']);
+				Swal.fire({
+					text: objects['message'],
+					icon: 'success',
+					confirmButtonText: 'OK'
+				}).then((result) => {
+					if (result.isConfirmed) {
+						window.location.href = './match.html';
+					}
+				});
+			} else {
+				Swal.fire({
+					text: objects['message'],
+					icon: 'error',
+					confirmButtonText: 'OK'
+				});
 			}
-		  });
-		} else {
-		  Swal.fire({
-			text: objects['message'],
-			icon: 'error',
-			confirmButtonText: 'OK'
-		  });
 		}
-	  }
 	};
 	return false;
-  }
+}
 
 document.addEventListener("DOMContentLoaded", function () {
 	var myNav = document.querySelectorAll(".sidenav");
--- server.js
+++ server.js
@@ -5,31 +5,31 @@ var cors = require("cors")
 let http = require('http').createServer(app);
 let io = require('socket.io')(http);
 
-app.use(express.static(__dirname+'/public'))
+app.use(express.static(__dirname + '/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors())
 
 
 io.on('connection', (socket) => {
-    console.log('a user connected', socket.id);
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
-    setInterval(()=>{
-      socket.emit('number', parseInt(Math.random()*10));
-    }, 1000);
-  
+  console.log('a user connected', socket.id);
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
   });
+  setInterval(() => {
+    socket.emit('number', parseInt(Math.random() * 10));
+  }, 1000);
 
-var port = process.env.port || 8080;
+});
+
+var port = process.env.port || 3000;
 
 
-app.use(express.static(__dirname+'/public'))
+app.use(express.static(__dirname + '/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
-http.listen(port,()=>{
-    console.log("Listening on port http://localhost/",port);
-  });
+http.listen(port, () => {
+  console.log("Listening on port http://localhost/", port);
+});
